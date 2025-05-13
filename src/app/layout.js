f8f12e3e9621ecf3408917a676dd12cf7566aa9d
Code <Code@MacBook-Pro-de-Martin.local>
2025-05-13 12:10:44 +0200

    full_screen test

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,12 +30,23 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <meta name="mobile-web-app-capable" content="yes"></meta>
+      {/* <meta name="mobile-web-app-capable" content="yes"></meta>
       <meta
         name="apple-mobile-web-app-status-bar-style"
         content="black-translucent"
       ></meta>
-      <meta name="theme-color" content="#000000"></meta>
+      <meta name="theme-color" content="#000000"></meta> */}
+
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
+      />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta
+        name="apple-mobile-web-app-status-bar-style"
+        content="black-translucent"
+      />
+      <meta name="theme-color" content="#000000" />
 
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen w-screen`}
